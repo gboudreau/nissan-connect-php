ee9e6044af153ee7c2c2903c887592b39a411f83
Guillaume Boudreau <github@danslereseau.com>
2016-02-21 15:57:47 -0500

    Added copyright information to .php class

--- NissanConnect.class.php
+++ NissanConnect.class.php
@@ -1,4 +1,21 @@
 <?php
+/*
+Copyright 2016 Guillaume Boudreau
+Source: https://github.com/gboudreau/nissan-connect-php
+
+This class is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This class is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Greyhole.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 class NissanConnect {
 
